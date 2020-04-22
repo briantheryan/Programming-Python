ffa3faabff3c56a09b85c449cf3916611c5e9c6e
Brian Rohde <b.rohde@student.rug.nl>
2020-04-22 21:36:03 +0200

    Finished first exercise (I believe)

--- Test.py
+++ Test.py
@@ -1,11 +1,10 @@
 """
 Exercises 11.1.12
 """
-
+import math
 
 class Point:
     """Test"""
-    import math
     def __init__(self, x=0, y=0):
         """Create a new point at x,y"""
         self.x = x
@@ -15,11 +14,11 @@ class Point:
         """Compute the distance between points."""
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
-    """def distance(p1, p2):
-        return math.sqrt( (p2.x-p1.x)**2 + (p2.y-p1.y)**2)
-"""
+def distance1(p1, p2):
+    return math.sqrt( (p2.x-p1.x)**2 + (p2.y-p1.y)**2)
 
 
-p = Point(5, 12)
-print(p.x)
-print(p.distance())
+p1 = Point(1,7)
+p2 = Point(5, 12)
+
+print(distance1(p1,p2))
