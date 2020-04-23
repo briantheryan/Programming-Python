1f265a92d66b7ca4681ed5ab0be1566152896882
Brian Rohde <b.rohde@student.rug.nl>
2020-04-23 12:43:10 +0200

    Difficulties comprehending methods

--- Test.py
+++ Test.py
@@ -1,24 +1,39 @@
 """
 Exercises 11.1.12
+1)
 """
 import math
 
+
 class Point:
     """Test"""
+
     def __init__(self, x=0, y=0):
         """Create a new point at x,y"""
         self.x = x
         self.y = y
 
-    def distance(self):
-        """Compute the distance between points."""
-        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+    def to_string(self):
+        return "({0}, {1})".format(self.x * -1, self.y)
 
-def distance1(p1, p2):
-    return math.sqrt( (p2.x-p1.x)**2 + (p2.y-p1.y)**2)
+    def reflect_x(self):
+        return((self.x * -1) + self.y
 
 
-p1 = Point(1,7)
+p1 = Point(1, 7)
 p2 = Point(5, 12)
 
-print(distance1(p1,p2))
+print(distance(p1, p2))
+print(p1.to_string())
+
+
+    """def distance(self):
+        return math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
+    """
+
+
+
+
+
+
+
