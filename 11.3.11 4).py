62896816f132ca3a22e82bcd6a55f153c278eec3
Brian Rohde <b.rohde@student.rug.nl>
2020-04-30 15:56:35 +0200

    Transfer of classes

--- 11.3.11 4).py
+++ 11.3.11 4).py
@@ -1,5 +1,5 @@
 from MyTime import MyTime
 t1 = MyTime(1,30,42)
-t2 = MyTime(2,45,55)
+t2 = MyTime(-1,-15,-7)
 
 print(t1.increment(t2))
---
+++ 11.4.10 1).py
@@ -0,0 +1,3 @@
+from Cards import Card
+card1 = Card(1,11)
+print(card1)
